d0207f7c7daa0606b7dfa2924154a05c6f761f5e
derekb123 <derekbutvin@gmail.com>
2020-07-22 00:25:40 +0000

    function that tests equality of 2 arrays and outputs assertions of their true or falseness

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const eqArrays = function(arr1, arr2) {
+  for (i = 0; i < arr1.length; i++) {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    else {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+
