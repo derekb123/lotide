6fb07c0394134a5c3e6352241f77dffed759bedf
derekb123 <derekbutvin@gmail.com>
2020-07-22 23:10:54 +0000

    created countLetters.js and made tests

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countLetters = function(str) {
+  countObj = {};
+  for (const char in str) {
+  //console.log(str[char]);
+    if (countObj[str[char]]) {
+      countObj[str[char]] += 1;
+    }
+    else {
+      countObj[str[char]] = 1;
+    }
+  }
+  return countObj;
+}
+
+assertEqual(countLetters('fluff').f, 3);
+assertEqual(countLetters('aardvark').a, 3);
+
+console.log(countLetters('abracadabra'));
