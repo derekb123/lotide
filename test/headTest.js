02b80a69a9068d53876ede94a2d668ddef86392e
derekb123 <derekbutvin@gmail.com>
2020-07-27 21:07:14 +0000

    corrected some bugs

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,5 @@
 const head = require('../head.js');
-const assertEqual = require('../head.js')
+const assertEqual = require('../assertEqual.js')
 
-assertEqual(head([]), 5);
+assertEqual(head([4,6]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
